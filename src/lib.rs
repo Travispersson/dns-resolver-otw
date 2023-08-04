14e2ee6731e0faa37b4fafe0fa1a399e93cfee1a
Travis Persson <travis.persson@viaplaygroup.com>
2023-08-04 09:48:08 +0200

    decode_name seems to work

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::{error::Error, io::Cursor};
+
 use rand::Rng;
 
 #[derive(Debug, Default)]
@@ -24,6 +26,22 @@ impl DNSHeader {
     }
 }
 
+impl TryFrom<&[u8]> for DNSHeader {
+    type Error = Box<dyn Error>;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        // Each of the 6 fields is a 2-byte integer, so there are 12 bytes in all to read.
+        Ok(DNSHeader {
+            id: u16::from_be_bytes(value[0..2].try_into()?),
+            flags: u16::from_be_bytes(value[2..4].try_into()?),
+            num_questions: u16::from_be_bytes(value[4..6].try_into()?),
+            num_answers: u16::from_be_bytes(value[6..8].try_into()?),
+            num_authorities: u16::from_be_bytes(value[8..10].try_into()?),
+            num_additionals: u16::from_be_bytes(value[10..12].try_into()?),
+        })
+    }
+}
+
 #[derive(Debug, Default)]
 struct DNSQuestion {
     name: Vec<u8>,
@@ -40,6 +58,64 @@ impl DNSQuestion {
         ]
         .concat()
     }
+}
+
+impl TryFrom<(Vec<u8>, &[u8])> for DNSQuestion {
+    type Error = Box<dyn Error>;
+
+    fn try_from((name, value): (Vec<u8>, &[u8])) -> Result<Self, Self::Error> {
+        Ok(DNSQuestion {
+            name,
+            type_: u16::from_be_bytes(value[2..4].try_into()?),
+            class: u16::from_be_bytes(value[4..6].try_into()?),
+        })
+    }
+}
+
+const UDP_DNS_RESPONSE_SIZE: usize = 1024;
+
+fn decode_name(
+    reader: &mut Cursor<&[u8; UDP_DNS_RESPONSE_SIZE]>,
+) -> Result<String, Box<dyn Error>> {
+    let mut length = reader.get_ref()[reader.position() as usize];
+    let mut decompressed_names: Vec<String> = vec![];
+
+    let mut current_pos = reader.position();
+    while length != 0 {
+        // check that first 2 bits are 1
+        if length & 0b11000000 != 0 {
+            let decoded_name = decode_compressed_name(reader)?;
+            decompressed_names.push(decoded_name);
+            current_pos += 2;
+            reader.set_position(current_pos);
+            return Ok(decompressed_names.join("."));
+        } else {
+            let start = (current_pos + 1) as usize;
+            let end = ((current_pos + length as u64) + 1) as usize;
+            let buf = reader.get_ref()[start..end].to_vec();
+            decompressed_names.push(String::from_utf8(buf)?);
+            current_pos += length as u64 + 1;
+            length = reader.get_ref()[current_pos as usize];
+        }
+    }
+
+    reader.set_position(current_pos + 1);
+    Ok(decompressed_names.join("."))
+}
+
+fn decode_compressed_name(
+    reader: &mut Cursor<&[u8; UDP_DNS_RESPONSE_SIZE]>,
+) -> Result<String, Box<dyn Error>> {
+    // takes the bottom 6 bits of the length byte, plus the next byte, and converts that to an integer called pointer
+    // saves our current position in reader
+    let pos = reader.position() as usize;
+    let parts = [
+        reader.get_ref()[pos] & 0b00111111,
+        reader.get_ref()[pos + 1],
+    ];
+    let pointer = u16::from_be_bytes(parts);
+    reader.set_position(pointer as u64);
+    decode_name(reader)
 }
 
 fn encode_dns_name(domain_name: &str) -> Vec<u8> {
@@ -92,7 +168,6 @@ mod tests {
         error::Error,
         net::{Ipv4Addr, UdpSocket},
     };
-    const UDP_DNS_RESPONSE_SIZE: usize = 1024;
 
     use super::*;
 
@@ -120,5 +195,23 @@ mod tests {
 
         // lets verify that some data exist in buffer for now with some shitty test haha
         assert_ne!(response_buffer[0], 0);
+    }
+
+    #[test]
+    fn test_decode_name() {
+        let mut data = [0; UDP_DNS_RESPONSE_SIZE];
+        let mut index = 0;
+        for p in "www.google.com".split('.') {
+            data[index] = p.len() as u8;
+            index += 1;
+            for c in p.chars() {
+                data[index] = c as u8;
+                index += 1;
+            }
+        }
+
+        let mut reader = Cursor::new(&data);
+        let decoded_name = decode_name(&mut reader).unwrap();
+        assert_eq!(decoded_name, "www.google.com");
     }
 }
